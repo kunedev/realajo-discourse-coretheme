cd65b0c5c4e6208e6aafb416265f21a367c20067
osullivan-sean <93526064+osullivan-sean@users.noreply.github.com>
2024-01-14 22:49:13 +0000

    connectors

---
+++ javascripts/discourse/widgets/r-customise-navbar-right.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var headerButtons = document.querySelector('.header-buttons');
+    if (headerButtons) {
+        headerButtons.innerHTML = '<i style="width: 15px; height:15px;" class="fa-sharp fa-solid fa-globe"></i>';
+    }
+});
+
