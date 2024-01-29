f67ebdb47a1ec7548eeb12bfd5188b58bc4f3a76
osullivan-sean <93526064+osullivan-sean@users.noreply.github.com>
2024-01-29 00:28:49 +0000

    raise-username-identity

--- javascripts/discourse/initializers/realajo-custom-pagenav.js
+++ javascripts/discourse/initializers/realajo-custom-pagenav.js
@@ -8,12 +8,21 @@ export default {
     withPluginApi("0.8.14", (api) => {
       api.onPageChange((url, title) => {
         
+        // Access current user data
+        const currentUser = api.getCurrentUser();
+
+        const currentUserName = currentUser ? currentUser.username : '';
+        window.DiscourseUser = currentUserName;
+
+    
         // Create a new event
         let event = new CustomEvent("RWrapperEvent", {
           detail: { eventReason: "pageChange",
              url: url, title: title }
         });
 
+        window.IsLoaded = true;
+        console.log("The DOM has loaded, window.IsLoaded is set, and window.DiscourseUser is set.");
         console.log("RWrapperEvent: pageChange raised");
 
         // Dispatch the event
