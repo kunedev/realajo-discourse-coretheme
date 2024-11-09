1dc0433b2e532ee58653f1f3948d1091720f7e1b
osullivan-sean <93526064+osullivan-sean@users.noreply.github.com>
2024-11-09 13:33:38 +0000

    t

--- javascripts/discourse/widgets/r-customise-navbar-right.js
+++ javascripts/discourse/widgets/r-customise-navbar-right.js
@@ -1,8 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOMContentLoaded loaded');
+    
+    /*
     var headerButtons = document.querySelector('.header-buttons');
     if (headerButtons) {
         headerButtons.innerHTML = '<div id="rdiv-header-buttons" class="nodisplay tooltip">   <i style="width: 15px; height:15px;" class="fa-sharp fa-solid fa-globe"></i>   <span class="tooltiptext">header-buttons [MODIFIED]</span> </div>';
     }
+    */  
+
+
 });
