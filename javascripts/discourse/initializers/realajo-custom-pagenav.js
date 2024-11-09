32617cf40f8d7e9fd3d37524aaac59418962b92a
osullivan-sean <93526064+osullivan-sean@users.noreply.github.com>
2024-11-09 20:43:42 +0000

    T

--- javascripts/discourse/initializers/realajo-custom-pagenav.js
+++ javascripts/discourse/initializers/realajo-custom-pagenav.js
@@ -2,7 +2,7 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 
 
 export default {
-  name: "application-wide-initializer",
+  name: "application-wide-initializer_vOLD",
 
   initialize() {
     withPluginApi("0.8.14", (api) => {
